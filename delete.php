08bf6093507f7405746acd699c99ffde874723a3
Hansen <jhanse12@dbdev.cs.kent.edu>
2019-12-02 18:36:23 -0500

    created insert and delete files

---
+++ delete.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Jonathan Hansen</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body id="hellspawn">
+		<!-- header -->	
+                <?php session_start(); ?>
+		<?php include 'nav.php'; ?>
+		<?php require 'database.php'; ?>
+		<?php include 'adminNav.php'; ?>
+		<?php include 'check_login.php'; ?>
+		<div>
+			<h1 align="center">Insert</h1>
+			<!-- rest of body -->
+			<div id="paraOne">
+				<p>Add 3 delete forms here (place functions in phpfunctions.php)</p>
+			</div>
+		</div>
+	</body>
+</html>
