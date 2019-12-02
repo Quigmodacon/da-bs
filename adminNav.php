87dccd2a51236cf1382d2911e007ca5f54ced830
Hansen <jhanse12@dbdev.cs.kent.edu>
2019-12-02 18:13:34 -0500

    fixed errors with nav bar stopping account table from showing up

--- adminNav.php
+++ adminNav.php
@@ -1,16 +1,15 @@
 <?php
 	$sql = $conn->prepare('SELECT isAdmin FROM user WHERE username = ?');
-	$sql->bind_param('s', $_SESSION['username']);
+	$username = $_SESSION['username'];
+	$sql->bind_param('s', $username);
 	$sql->execute();
-	$result = $sql->get_result();
-	if($result->num_rows > 0) {
-		$row = $result->fetch_assoc();
-		if($row['isAdmin'] == 1) {
-
+	$sql->bind_result($isAdmin);
+	if($sql->fetch()) {
+		if($isAdmin > 0) {
 			echo '<div class="topnav">';
-				echo '<a href="insert.php">Insert</a>';
-				echo '<a href="delete.php">Delete</a>';
-				echo '<a href="update.php">Update User</a>';
+			echo '<a href="insert.php">Insert</a>';
+			echo '<a href="delete.php">Delete</a>';
+			echo '<a href="update.php">Update User</a>';
 			echo '</div>';
 		}
 	}
