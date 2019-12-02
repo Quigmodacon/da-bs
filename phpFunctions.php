db52f8af80b7259961a3b0763537a28b8dce87f5
Hansen <jhanse12@dbdev.cs.kent.edu>
2019-12-02 11:18:23 -0500

    Updated display function for organism

--- phpFunctions.php
+++ phpFunctions.php
@@ -1,10 +1,10 @@
 <?php
 
-function show_instructor($conn) {
+function show_organism($conn) {
 
 	//include "dbconnect.php";
 
-	$sql = "SELECT id, name, dept_name, salary FROM instructor";
+	$sql = "SELECT id, name, dept_name, salary FROM organism";
 	$result = $conn->query($sql); // object oriented execution of query
 
 		if ($result->num_rows > 0) {
@@ -13,16 +13,16 @@ function show_instructor($conn) {
 			
 			echo '<table border>';
 			echo '<thead><tr>';
-			echo '<th>'."ID".'</th>'.'<th>'."Name".'</th>'.'<th>'."Department".'</th>'.'<th>'."Salary".'</th>';
+			echo '<th>'."ID".'</th>'.'<th>'."Name".'</th>'.'<th>'."Scientific Name".'</th>'.'<th>'."Organism Type".'</th>';
 			echo '</tr></thead>';
 			echo '<tbody>';
 
 			while($row = $result->fetch_assoc()) {
 				echo '<tr>';
-				echo "<td>" . $row["id"]. "</td>";
-				echo "<td>" . $row["name"]. "</td>";
-				echo "<td>" . $row["dept_name"]. "</td>";
-				echo "<td>" . $row["salary"]. "</td>";
+				echo "<td>" . $row["organismID"]. "</td>";
+				echo "<td>" . $row["orgName"]. "</td>";
+				echo "<td>" . $row["sciName"]. "</td>";
+				echo "<td>" . $row["orgType"]. "</td>";
 				echo '</tr>';
 			}
 			
