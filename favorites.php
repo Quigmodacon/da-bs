a6ac5c09e84de39590bebe04ede83bdf34051ff0
Hansen <jhanse12@dbdev.cs.kent.edu>
2019-12-04 20:54:23 -0500

    fixed favorites styling

--- favorites.php
+++ favorites.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Jonathan Hansen</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body id="hellspawn">
@@ -12,6 +13,7 @@
 		<?php require 'phpFunctions.php'?>
 		<?php require 'database.php' ?>
 	        <?php include 'check_login.php'; ?>
+		<main class="container" role="main">
 		<div>
 			<h1 align="center">Favorites</h1>
 			<!-- rest of body -->
@@ -34,5 +36,9 @@
 				<input type="submit" value="Add New Favorite List">
 			</form>
 		</div>
+		</main>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	</body>
 </html>
